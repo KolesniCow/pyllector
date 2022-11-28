bafdf15f74e793aad0a940cee55ba974636167f8
KolesniCow <billybool@billy-debian.billy>
2022-11-28 19:26:39 +0300

    rewritting method push, add content type test

--- api_collector.py
+++ api_collector.py
@@ -25,13 +25,6 @@ class ApiCollector:
             if content_type == ContentType.JSON:
                 return await response.json
 
-    # def get_json_content(self, method: str, http_method: HttpMethod, params: dict = None) -> dict:
-    #     return self.push_request(method, http_method, params).json()
-
-    # def get_html_content(self, method: str, http_method: HttpMethod, params: dict = None) -> BeautifulSoup:
-    #     html = self.push_request(method, http_method, params).text
-    #     return BeautifulSoup(html, 'html.parser')
-
     async def get_until_http_code(self, method: str, http_method: HttpMethod,
                             limit: int = 5, time: float = 10,
                             params: dict = None, http_code: int = 200) -> str | None | dict:
